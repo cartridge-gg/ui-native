f2549ebba7ed5bea4de3a64e33d1c28dbf2e9eb3
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-06-25 12:15:48 +0200

    Export primitives from its index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,2 @@
-// Toast functionality
-
-// Icons
 export * from "./icons";
-export * from "./primitives/badge";
-// Core primitives
-export * from "./primitives/button";
-export * from "./primitives/card";
-export * from "./primitives/label";
-export * from "./primitives/separator";
-export * from "./primitives/skeleton";
-export * from "./primitives/sonner";
-export * from "./primitives/spinner";
-export * from "./primitives/text";
+export * from "./primitives";
