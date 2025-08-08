cad4a4bbaea03f522479995ab2bebc0d4d97301f
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-08-08 12:36:47 +0200

    Initialize CartridgeAccount via wasm2js

--- app/wasm2js.tsx
+++ app/wasm2js.tsx
@@ -1,7 +1,14 @@
 import { View } from "react-native";
 import { Text } from "#components";
+import { CartridgeAccount } from "#controller/account_wasm";
+import { useEffect } from "react";
 
 export default function Wasm2Js() {
+  useEffect(() => {
+    const account = CartridgeAccount.new("1", "1", "1", "1", "1", "1", { signer: undefined, account: "1" }, "1");
+    console.log(account);
+  }, []);
+
   return (
     <View>
       <Text>Wasm2Js</Text>
