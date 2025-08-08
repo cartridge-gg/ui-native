31a4caac136b4781bb9e4c4efc5d80a0f9808fa9
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-08-08 11:42:54 +0200

    Add wasm2js test screen

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,16 @@ import { View } from "react-native";
 import { Button, SonnerToaster, Text } from "#components";
 
 export default function RootScreen() {
-	return (
-		<View className="flex-1 items-center justify-center bg-background-100 gap-4">
-			<Text className="text-2xl font-bold">Cartridge Marketplace</Text>
-			<Button onPress={() => router.push("/connect")}>
-				<Text>Connect</Text>
-			</Button>
-			<SonnerToaster />
-		</View>
-	);
+  return (
+    <View className="flex-1 items-center justify-center bg-background-100 gap-4">
+      <Text className="text-2xl font-bold">Cartridge Marketplace</Text>
+      <Button onPress={() => router.push("/connect")}>
+        <Text>Connect</Text>
+      </Button>
+      <Button onPress={() => router.push("/wasm2js")}>
+        <Text>Wasm2Js</Text>
+      </Button>
+      <SonnerToaster />
+    </View>
+  );
 }
---
+++ app/wasm2js.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "#components";
+
+export default function Wasm2Js() {
+  return (
+    <View>
+      <Text>Wasm2Js</Text>
+    </View>
+  );
+}
