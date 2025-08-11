e81c4698a0e10c42573fec993f5a81d1b74f2d0f
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-08-11 14:50:24 +0200

    Open keychain with expo web browser

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { router } from "expo-router";
 import { View } from "react-native";
 import { Button, SonnerToaster, Text } from "#components";
+import { KEYCHAIN_URL } from "#utils";
+import * as WebBrowser from "expo-web-browser";
 
 export default function RootScreen() {
-	return (
-		<View className="flex-1 items-center justify-center bg-background-100 gap-4">
-			<Text className="text-2xl font-bold">Cartridge Marketplace</Text>
-			<Button onPress={() => router.push("/connect")}>
-				<Text>Connect</Text>
-			</Button>
-			<SonnerToaster />
-		</View>
-	);
+  const onConnect = async () => {
+    try {
+      const res = await WebBrowser.openBrowserAsync(KEYCHAIN_URL);
+      console.log(res);
+    } catch (error) {
+      console.error("Error opening browser:", error);
+    }
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center bg-background-100 gap-4">
+      <Text className="text-2xl font-bold">Cartridge Marketplace</Text>
+      <Button onPress={onConnect}>
+        <Text>Connect</Text>
+      </Button>
+      <SonnerToaster />
+    </View>
+  );
 }
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,3 @@
 export const CARTRIDGE_API_URL = "https://api.cartridge.gg/query";
+
+export const KEYCHAIN_URL = "https://x.cartridge.gg"
