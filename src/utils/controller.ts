66aeae9b556ca6e1cfe02e5f1cdf67e8f7b5148f
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-08-18 18:34:46 +0200

    Test receiving data from keychain via deeplink

--- src/utils/controller.ts
+++ src/utils/controller.ts
@@ -2,6 +2,7 @@ import * as WebBrowser from "expo-web-browser";
 import { KEYCHAIN_URL } from "#utils/const";
 import { RequestFn, StarknetWindowObject } from "@starknet-io/types-js";
 import { icon } from "#utils/icon";
+import * as Linking from "expo-linking";
 
 export class MobileProvider implements StarknetWindowObject {
 	public id = "controller_mobile";
@@ -13,7 +14,10 @@ export class MobileProvider implements StarknetWindowObject {
 
 	async connect() {
 		try {
-			const res = await WebBrowser.openAuthSessionAsync(KEYCHAIN_URL);
+      const redirectUrl = Linking.createURL("");
+      const authUrl = `${KEYCHAIN_URL}?callback_url=${encodeURIComponent(redirectUrl)}`;
+      console.log(authUrl, redirectUrl);
+			const res = await WebBrowser.openAuthSessionAsync(authUrl, redirectUrl);
 			console.log(res);
 		} catch (error) {
 			console.error("Error opening browser:", error);
