ae9d570d5a6700b88cb02b398c083b0280a9b6ba
JunichiSugiura <jun.sugiura.jp@gmail.com>
2025-09-29 15:48:29 +0200

    Limit mock data to avoid bad performance

--- src/clone/arcade/context/discovers.tsx
+++ src/clone/arcade/context/discovers.tsx
@@ -2,39 +2,39 @@ import { createContext, type ReactNode } from "react";
 import mockedValue from "./discover.mock.json";
 
 export type Discover = {
-  identifier: string;
-  project: string;
-  callerAddress: string;
-  start: number;
-  end: number;
-  count: number;
-  actions: string[];
-  achievements: {
-    title: string;
-    icon: string;
-    points: number;
-  }[];
+	identifier: string;
+	project: string;
+	callerAddress: string;
+	start: number;
+	end: number;
+	count: number;
+	actions: string[];
+	achievements: {
+		title: string;
+		icon: string;
+		points: number;
+	}[];
 };
 
 export type DiscoversContextType = {
-  playthroughs: { [key: string]: Discover[] };
-  usernames: { [key: string]: string | undefined };
-  status: "success" | "error" | "idle" | "loading";
+	playthroughs: { [key: string]: Discover[] };
+	usernames: { [key: string]: string | undefined };
+	status: "success" | "error" | "idle" | "loading";
 };
 
 const initialState = {
-  ...mockedValue,
-  status: "success",
+	...mockedValue,
+	status: "success",
 } as DiscoversContextType;
 
 export const DiscoversContext = createContext<DiscoversContextType | null>(
-  initialState,
+	initialState,
 );
 
 export function DiscoversProvider({ children }: { children: ReactNode }) {
-  return (
-    <DiscoversContext.Provider value={initialState}>
-      {children}
-    </DiscoversContext.Provider>
-  );
+	return (
+		<DiscoversContext.Provider value={initialState}>
+			{children}
+		</DiscoversContext.Provider>
+	);
 }
